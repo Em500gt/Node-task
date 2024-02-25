27303226f722dfd64dd4d050193c4f246cf3f3ee
Egor Malinovski <Em500gt@gmail.com>
2024-02-25 11:35:30 +0300

    Add task1

---
+++ Task1/main.js
@@ -0,0 +1,6 @@
+const mathFunc = require('./math');
+
+console.log(mathFunc.sum(10, 5));
+console.log(mathFunc.minus(10, 5));
+console.log(mathFunc.division(10, 5));
+console.log(mathFunc.multiply(10, 5));
---
+++ Task1/math.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const division = (a, b) => a / b;
+
+module.exports = {
+    sum, minus, multiply, division
+}
