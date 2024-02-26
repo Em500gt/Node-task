b02f2a8ec0150ed1bf5d0025dba74c5d045858c5
Egor Malinovski <Em500gt@gmail.com>
2024-02-26 22:11:07 +0300

    add logger and update server.js

--- Task2/server.js
+++ Task2/server.js
@@ -6,70 +6,73 @@ app.use(express.json())
 
 const users = [];
 
-app.get('/api/hello', (req, res) => {
+function logger(req, res, next) {
+    console.log(`Запрос на ${req.url}`)
+    next();
+}
+
+app.get('/api/hello', logger, (req, res) => {
     res.send("Привет, Redev!");
 })
 
-app.post('/api/echo', (req, res) => {
+app.post('/api/echo', logger, (req, res) => {
     const message = req.body.message;
     res.json({ echo: message });
 })
 
-app.post('/api/users', (req, res) => {
-    const { id, username, email, password } = req.body;
-    users.push({ id, username, email, password });
-    res.send(`Пользователь ${username} -> создан!`)
-})
+app.route('/api/users')
+    .post(logger, (req, res) => {
+        const { id, username, email, password } = req.body;
+        users.push({ id, username, email, password });
+        res.send(`Пользователь ${username} -> создан!`)
+    })
+    .get(logger, (req, res) => {
+        res.send(users);
+    })
 
-app.get('/api/users', (req, res) => {
-    res.send(users);
-})
+app.route('/api/users/:id')
+    .get(logger, (req, res) => {
+        const userId = parseInt(req.params.id);
+        const result = users.find((item) => item.id === userId);
+        result ? res.send(result) : res.send("Пользователь по данному ID не найден!")
+    })
+    .put(logger, (req, res) => {
+        const userId = parseInt(req.params.id);
+        const { username, email, password } = req.body;
+        const user = users.find((user) => user.id === userId);
 
-app.get('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const result = users.find((item) => item.id === userId);
-    result ? res.send(result) : res.send("Пользователь по данному ID не найден!")
-})
+        if (!user) {
+            return res.send("Пользователь не найден!");
+        }
 
-app.put('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const { username, email, password } = req.body;
-    const user = users.find((user) => user.id === userId);
+        user.username = username || user.username;
+        user.email = email || user.email;
+        user.password = password || user.password;
+        res.send("Информация о пользователе обновлена!");
 
-    if (!user) {
-        return res.send("Пользователь не найден!");
-    }
+    })
+    .patch(logger, (req, res) => {
+        const userId = parseInt(req.params.id);
+        const user = users.find((user) => user.id === userId);
 
-    user.username = username || user.username;
-    user.email = email || user.email;
-    user.password = password || user.password;
-    res.send("Информация о пользователе обновлена!");
+        if (!user) {
+            return res.send("Пользователь не найден!");
+        }
 
-})
+        user.password = req.body.password || user.password;
+        res.send(`Пароль пользователя обновлен!`);
+    })
+    .delete(logger, (req, res) => {
+        const userId = parseInt(req.params.id);
+        const userIndex = users.findIndex((user) => user.id === userId);
 
-app.patch('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find((user) => user.id === userId);
+        if (userIndex === -1) {
+            return res.send("Пользователь не найден!");
+        }
 
-    if (!user) {
-        return res.send("Пользователь не найден!");
-    }
-
-    user.password = req.body.password || user.password;
-    res.send(`Пароль пользователя обновлен!`);
-})
-
-app.delete('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === userId);
-
-    if (userIndex === -1) {
-        return res.send("Пользователь не найден!");
-    }
-
-    users.splice(userIndex, 1)
-    res.send("Пользователь был удален");
-})
+        users.splice(userIndex, 1)
+        res.send("Пользователь был удален");
+    })
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
