883684cb4da87488a6a925d0d67b4568263bdd98
Egor Malinovski <Em500gt@gmail.com>
2024-04-30 14:33:38 +0300

    add task4

--- Task3/src/server.js
+++ Task3/src/server.js
@@ -3,7 +3,6 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.MY_PORT;
 app.use(express.json());
-
 const router = require('./routes');
 const logger = require('./helpers/logger');
 app.use('/api', logger, router);
